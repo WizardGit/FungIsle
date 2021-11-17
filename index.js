86edf244df5ffcf403b5988a0108109ff8e81e0d
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-17 12:13:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function backup()
     location.href = 'SQLDump.php';
 }*/
 window.addEventListener("beforeunload", function(e){
+    
+    console.log("oka");
+    this.alert("we go");
     location.href = 'SQLDump.php';
+    this.alert("We done");
+    console.log("oka");
  }, false);
