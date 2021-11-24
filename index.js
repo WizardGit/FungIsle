64393b94dcac1b7aaf2db69fd63bc63205abc38c
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 12:57:49 -0800

    Big renovation

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = init();
+function init()
+{
+    
+}
+
+function attack()
+{
+    window.location.href="./phps/attack.php";
+}
---
+++ phps/resetDB.php
@@ -0,0 +1,50 @@
+<!--
+Author: Kaiser
+Original Author: Chris
+Date last edited: 11/15/2021
+-->
+
+<?php
+include('connectionDataStrong.txt');
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+?>
+
+<html lang="en">
+<head>
+  <title>Reset Database</title>
+  <link rel="stylesheet" href="displayStyle.css?v=<?php echo time(); ?>"> 
+  </head>  
+  <body>
+    <section>
+      <a href="index.html">Back to Home Page</a>
+      <p> <h2>The query:</h2> <p>
+      <?php
+        $query = "UPDATE Human h         SET h.health=99         WHERE SaladSN > 4;";
+        $queryAtt = "select * from Human h";
+        print $query;
+      ?>
+
+      <p> <h2>Result of query:</h2> <p>
+
+      <?php
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        $resultAtt = mysqli_query($conn,$queryAtt) or die(mysqli_error($conn));
+
+        print "<pre>";
+
+        while($row = mysqli_fetch_array($resultAtt, MYSQLI_ASSOC))
+        {  
+          
+          foreach ($row as $element)
+          printf("[%- 8s]",$element);   
+          print "<br>";
+        }
+
+        print "</pre>";
+        mysqli_free_result($result);
+        mysqli_close($conn);
+      ?>
+    </section>
+  </body>
+</html>	  
