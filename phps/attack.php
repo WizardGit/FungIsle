9ea362366bc8a43afe8e6c2e23ab78d20409f7a4
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 15:57:43 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -68,6 +68,7 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
             $query = "select count(*) as total from Human h where h.role='Henchman' group by h.role";
             $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+            $totDmg = $row[0];  
             echo "Hello world!";
         }
         
