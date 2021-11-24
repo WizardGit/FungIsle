0dbb0eff1c77edf78581093789d26ff4995fda7b
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 15:48:19 -0800

    condensing code into functions

--- phps/attack.php
+++ phps/attack.php
@@ -21,13 +21,11 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
       <?php
         //Count soldiers then multiply that number by 1
         $village = $_POST['village'];
-        $query1 = "select count(*) as total from Human h where h.role='Henchman' group by h.role";
+        
 
         //Get name of hero and dmg and hp        
         $hero = $_POST['hero'];
-        $query2 = "select h.firstName, h.health, w.attack from Human h inner join Weapon w on h.Weapon_Name=w.Name 
-        where h.firstName=";
-        $query2 = $query2."'".$hero."';";
+        
 
         //Display current human status
         $queryAtt = "select * from Human h";
@@ -37,63 +35,39 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
         /* Choose random soldier and decrease their hp by dmg of hero*/
 
         /* Get all the soldiers with their weapons*/
-        printf("Hero: %s\n",$hero);
-        printf("Village: %s\n", $village);
+        printf("Hero: %s\n\n",$hero);
+        printf("Village: %s\n\n", $village);
 
-        printf("QR1: %s\n", $query1);
-        printf("QR2: %s\n", $query2);
+        printf("QR1: %s\n\n", $query1);
+        printf("QR2: %s\n\n", $query2);
       ?>
 
       <p> <h2>Result of query:</h2> <p>
 
       <?php
-      $totDmg = 0;
-      $newHeroHp = 0;
-      $dmgFromHero = 0;
-        //q1
-        $result1 = mysqli_query($conn, $query1) or die(mysqli_error($conn));
-        print "<pre>";
-        while($row = mysqli_fetch_array($result1, MYSQLI_ASSOC))
-        {  
-          
-          foreach ($row as $element)
-          {
-            printf("[%- 8s]",$element); 
-            $totDmg += $element;
-          }  
-          print "<br>";
-        }
-        print "</pre>";
-        mysqli_free_result($result1);
-        printf("Total Dmg to Hero: %s\n", $totDmg);
-        //end q1
 
-        //q2
-        $result2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
-        print "<pre>";
-        while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
-        {  
-          $counter = 0;
-          foreach ($row as $element)
-          {
-            printf("[%- 8s]",$element);
-            $counter++;
-            if ($counter == 2)
-            {
-                $newHeroHp -= $element;
-            }
-            else if ($counter == 3)
-            {
-                $dmgFromHero = $element;
-            }
-          }   
-          print "<br>";
-        }
-        print "</pre>";
-        mysqli_free_result($result2);
-        printf("Hero's new HPo: %s\n", $newHeroHp);
-        printf("Dmg from Hero: %s\n", $dmgFromHero);
-        //end q2
+    function getNumHenchmenIn($village)
+    {     
+        $query = "select count(*) as total from Human h where h.role='Henchman' group by h.role";
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+        $totDmg = $row[0];  
+        mysqli_free_result($result);
+        printf("getNumHenchmenIn: %s\n", $totDmg);
+    }
+    
+    function reduceHeroHealth($dmg)
+    {
+        $query = "select h.health from Human h where h.firstName=";
+        $query = $query."'".$hero."';";        
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        $row = mysqli_fetch_array($result2, MYSQLI_ASSOC));  
+        $newHp =  $row[0] - $dmg;       
+        mysqli_free_result($result);
+        printf("Hero's new HP should be: %s\n", $newHp);    
+    }
+
+        
 
         
         //Print results
@@ -110,7 +84,7 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
         mysqli_free_result($result);        
         //Print results end
 
-
+        reduceHeroHealth(10);
         mysqli_close($conn);
         
         // Subtract from one soldier
