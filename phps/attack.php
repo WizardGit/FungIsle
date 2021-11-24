f9f901c21449e90b76de4bb8fd5dacc8c8dbb3c4
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 15:18:47 -0800

    changes to attack php

--- phps/attack.php
+++ phps/attack.php
@@ -19,18 +19,18 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
       <a href="../index.html">Back to Home Page</a>
       <p> <h2>The query:</h2> <p>
       <?php
-        $hero = $_POST['hero'];
-        $village = $_POST['village'];
-        $query = $_POST['query'];
         //Count soldiers then multiply that number by 1
+        $village = $_POST['village'];
         $query1 = "select count(*) as total from Human h where h.role='Henchman' group by h.role";
-        //Get name of hero and dmg and hp
+
+        //Get name of hero and dmg and hp        
+        $hero = $_POST['hero'];
         $query2 = "select h.firstName, h.health, w.attack from Human h inner join Weapon w on h.Weapon_Name=w.Name 
         where h.firstName=";
         $query2 = $query2."'".$hero."';";
 
+        //Display current human status
         $queryAtt = "select * from Human h";
-
 
         /* Get all the soldiers with their weapons*/
         /* Subtract the dmg from heros hp*/
@@ -39,7 +39,6 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
         /* Get all the soldiers with their weapons*/
         printf("Hero: %s\n",$hero);
         printf("Village: %s\n", $village);
-        printf("Query: %s\n", $query);
 
         printf("QR1: %s\n", $query1);
         printf("QR2: %s\n", $query2);
@@ -48,12 +47,40 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
       <p> <h2>Result of query:</h2> <p>
 
       <?php
+        //q1
         $result1 = mysqli_query($conn, $query1) or die(mysqli_error($conn));
-        $result2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
-        $resultAtt = mysqli_query($conn,$queryAtt) or die(mysqli_error($conn));
-
         print "<pre>";
+        while($row = mysqli_fetch_array($result1, MYSQLI_ASSOC))
+        {  
+          
+          foreach ($row as $element)
+          printf("[%- 8s]",$element);   
+          print "<br>";
+        }
+        print "</pre>";
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        //end q1
 
+        //q2
+        $result2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
+        print "<pre>";
+        while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
+        {  
+          
+          foreach ($row as $element)
+          printf("[%- 8s]",$element);   
+          print "<br>";
+        }
+        print "</pre>";
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        //end q2
+
+        
+        //Print results
+        $resultAtt = mysqli_query($conn,$queryAtt) or die(mysqli_error($conn));
+        print "<pre>";
         while($row = mysqli_fetch_array($resultAtt, MYSQLI_ASSOC))
         {  
           
@@ -61,10 +88,17 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
           printf("[%- 8s]",$element);   
           print "<br>";
         }
-
         print "</pre>";
         mysqli_free_result($result);
         mysqli_close($conn);
+        //Print results end
+
+
+
+        
+        // Subtract from one soldier
+
+        // Subtract from hero
       ?>
     </section>
   </body>
