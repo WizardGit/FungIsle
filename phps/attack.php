2d6a6c79a3b60000235a30f494931f104373cb1b
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 16:14:43 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -42,7 +42,7 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
       <?php
 
         getNumHenchmenIn($conn, "Northland");
-        reduceHeroHealth($conn, 10, $hero);
+        reduceHeroHealth($conn, $hero, 10);
 
         //Print results
         $resultAtt = mysqli_query($conn,$queryAtt) or die(mysqli_error($conn));
