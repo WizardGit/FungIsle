4153e5e7d36da5e4403dc366b822c2fb9ea358be
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-24 16:07:56 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -79,8 +79,9 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
         
         function reduceHeroHealth($conn, $dmg)
         {
-            $query = "select h.health from Human h where h.firstName=";
-            $query = $query."'".$hero."';";        
+            $newHp = 0;
+            $query = "select h.health from Human h where h.firstName='Mushronian'";
+            //$query = $query."'".$hero."';";        
             $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC));  
             foreach ($row as $element)
