0e9d5e15e22ca5bf219a03d3dac2aa037fee58c7
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-26 18:27:42 -0800

    ditched idea

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ Purpose: Empty Shell
 window.onload = init();
 function init()
 {
-    document.cookie = "username=John Doe";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    let c = ca[ca.length-1];
-    console.log(c);
+    
 }
 
-function chg()
-{
-    document.cookie = "username=John Doe";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    let c = ca[ca.length-1];
-    console.log(c);
-}
