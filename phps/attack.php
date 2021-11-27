452bfbd7d3881b36c5c094f2047a15124098c1c6
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-26 17:03:30 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -354,7 +354,6 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
           $query = $query."'".$hero."';"; 
           $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-          $hello = $row['name'];
           printf("%s is located at %s <br>", $hero, $row['name']);
           
           return $row['name'];
