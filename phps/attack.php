286975fecee1b275a762246359e662914bcba40e
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-26 18:45:45 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -350,7 +350,7 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
           $query = $query."'".$hero."';"; 
           $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-          if (strcmp($row['Village_ID'], $village) != 0)
+          if ($row['Village_ID'] != $village)
           {
             printf("%s drives their %s to %s <br>", $hero, $row['Type'], $village);
           }         
