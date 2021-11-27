610a641e26072c6df6cd02abf147836bbc286ca1
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-26 18:48:57 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -346,7 +346,9 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
           $newVID = $row['VillageID'];
 
-          $query = "select v.Type, v.Name from Vehicle v inner join Human h on h.SaladSN=v.Human_SaladSN inner join Village vl on h.Village_ID=v.VillageID where h.firstName="; 
+          $query = "select v.Type, v.Name from Vehicle v 
+          inner join Human h on h.SaladSN=v.Human_SaladSN 
+          inner join Village vl on h.Village_ID=vl.VillageID where h.firstName="; 
           $query = $query."'".$hero."';"; 
           $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
