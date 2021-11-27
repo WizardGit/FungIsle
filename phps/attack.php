b4a72be76a42d891c1944ae905cc34e342436473
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-26 17:01:57 -0800

    Update attack.php

--- phps/attack.php
+++ phps/attack.php
@@ -356,7 +356,7 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
           return $row['name'];
           mysqli_free_result($result);
-          printf("%s is located at %s <br>"), $hero, $row['name']);
+          printf("%s is located at %s <br>");
         }
 
         function checkVillageStatus($conn, $village)
