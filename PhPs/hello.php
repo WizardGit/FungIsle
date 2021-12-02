1b309ff2f6d96a399c8f3acb686bbba2c8c38db7
Kaiser Slocum <slocumkaiser@gmail.com>
2021-12-01 18:55:19 -0800

    testing

---
+++ PhPs/hello.php
@@ -0,0 +1,12 @@
+<?php include 'first.php';?>
+<div id='display_form_div'>
+<?php attack($conn); ?>
+</div> 
+<?php include 'last.php';?>
+<?php
+function attack($conn)
+{
+  printf("hello");
+}
+
+      ?>
